158a6a751c1d3cd25f8a5c2318c2b0a2443ea6b9
Mikhail Stralenia <mstralenya@gmail.com>
2024-11-04 16:12:59 +0100

    updated gitignore

--- configuration.nix
+++ configuration.nix
@@ -45,7 +45,7 @@
       device = "//192.168.8.1/disk";
       fsType = "cifs";
       options = let automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
-      in ["${automount_opts},credentials=/etc/samba/smb-secrets,uid=1000,gid=100"];
+      in ["${automount_opts},credentials=smb-secrets,uid=1000,gid=100"];
     };
 
     # Set your time zone.
